cfeeaf1079976ca56e278bf508d03d0a061eade7
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-07-26 15:45:21 -0700

    Time: 178 ms (34.18%), Space: 17.5 MB (10.65%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,37 @@
+class Solution:
+    
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        adjList={i:[]  for i in range(numCourses)}
+        
+        for c,p in prerequisites:
+            adjList[c].append(p)
+        
+        
+        def dfs(i):
+            visited.add(i)
+            dfsvisited.add(i)
+            
+            for nei in adjList[i]:
+                if nei not in visited:
+                    if(dfs(nei)):
+                        return True
+                   
+                elif nei in dfsvisited:
+                    return True
+            
+            dfsvisited.remove(i)
+        
+        visited=set()
+        dfsvisited=set()
+        
+        for i in adjList:
+            if i not in visited:
+                if(dfs(i)):
+                    return False
+               
+           
+        return True
+            
+        
+      
