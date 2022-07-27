47f143591b2ef571a5d369c7b039d0653462b84c
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-07-27 15:01:19 -0700

    Time: 4479 ms (32.78%), Space: 175 MB (10.08%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,50 +1,50 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         
-        #MST so we will ise prism algorithm
+        #MST so we will USE PRISM algorithm
+        
         
         adjList={i:[] for i in range(len(points))}
         
-        
         for i in range(len(points)):
             x1,y1=points[i]
-            
             for j in range(i+1,len(points)):
                 x2,y2=points[j]
-                
                 distance=abs(x1-x2)+abs(y1-y2)
                 adjList[i].append([distance,j])
                 adjList[j].append([distance,i])
-               
-           
+                
+        
+        visited=set()
         minheap=[]
         heapq.heapify(minheap)
         
         minheap.append([0,0])
         
-        visited=set()
         res=0
-        
         while(len(visited)!=len(points)):
+            
             distance,node=heapq.heappop(minheap)
-
+            
             if node in visited:
                 continue
-            visited.add(node)
+               
             res+=distance
+            visited.add(node)
             
-            for distance,nei in adjList[node]:
+            for dist,nei in adjList[node]:
                 if nei not in visited:
-                    heapq.heappush(minheap,[distance,nei])
-
-            
-        return res 
-            
-            
-            
-            
+                    heapq.heappush(minheap,[dist,nei])
+                    
+                
+        return res
             
             
         
         
         
+        
+        
+        
+        
+       
