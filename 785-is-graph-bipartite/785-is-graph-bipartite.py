953d75d6229fa12db69bb0d589936721f6286344
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-07-30 18:21:40 -0700

    Time: 186 ms (92.29%), Space: 14.6 MB (29.05%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -2,26 +2,25 @@ class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         
         
-        
         def dfs(i,color):
+            
             if i in colors:
                 return not colors[i]==color
             
             colors[i]=color
             
             for nei in graph[i]:
-                    if dfs(nei,1-color):
-                        return True
+                if dfs(nei,1-colors[i]):
+                    return True
             
-            
+        
         
         colors={}
-        
         for i in range(len(graph)):
             if i not in colors:
                 if dfs(i,0):
                     return False
-                
-           
+               
         return True
+        
         
