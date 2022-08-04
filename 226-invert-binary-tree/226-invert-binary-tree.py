d6267355ea2d2fb192d2541d0d64a8e7d2807ae5
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-08-03 18:22:35 -0700

    Time: 46 ms (57.86%), Space: 13.9 MB (11.96%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -11,10 +11,8 @@ class Solution:
             return 
         
         root.left,root.right=root.right,root.left
-        
         self.invertTree(root.left)
         self.invertTree(root.right)
-        
         
         return root
         
