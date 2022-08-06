008e0c364fdf3d949ad77a461408716fa9c72720
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-08-06 13:31:14 -0700

    Time: 36 ms (83.33%), Space: 14 MB (29.67%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -11,22 +11,29 @@ class Solution:
         
         def inorder(root):
             if not root:
-                return ['A']
+                return ['10000']
             return [*inorder(root.left),root.val,*inorder(root.right)]
-            
+        
+        
         def preorder(root):
             if not root:
-                return ['A']
+                return ['10000']
             return [root.val,*preorder(root.left),*preorder(root.right)]
+        
         
         res1=inorder(p)
         res2=inorder(q)
-        
         res3=preorder(p)
         res4=preorder(q)
         
-
+        print(res1)
+        print(res2)
+        print(res3)
+        print(res4)
+        
         return res1==res2 and res3==res4
         
-   
-      
+        
+        
+        
+        
