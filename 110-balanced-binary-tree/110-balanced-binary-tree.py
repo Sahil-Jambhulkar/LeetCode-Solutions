f6919083b70d6f5e98c86212cc5d99523b955c9c
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-08-06 16:54:16 -0700

    Time: 60 ms (85.54%), Space: 18.9 MB (11.40%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -16,17 +16,15 @@ class Solution:
             left=dfs(root.left)
             right=dfs(root.right)
             
+            bal=left[0] and right[0] and abs(left[1]-right[1])<=1
             
-            balanced=left[0] and right[0] and abs(left[1]-right[1])<=1
-            
-            return [balanced,1+max(left[1],right[1])]
+            return [bal,1+max(left[1],right[1])]
        
-        
-        
+    
         return dfs(root)[0]
+    
             
             
-            
-            
+        
         
         
