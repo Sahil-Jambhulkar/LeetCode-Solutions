6734903c6845e962d4cdc0d236cdd216dbc03639
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-08-05 23:03:38 -0700

    Time: 45 ms (59.59%), Space: 13.9 MB (13.06%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -7,32 +7,10 @@
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         
-        
-        #iterative DFS
-        
         if not root:
             return []
         
-        
-        stack=[root]
-        res=[]
+        return [root.val,*self.preorderTraversal(root.left),*self.preorderTraversal(root.right)]
         
         
-        while(stack):
-            node=stack.pop()
-            
-            res.append(node.val)
-            
-            if node.right:
-                stack.append(node.right)
-               
-            if node.left:
-                stack.append(node.left)
-               
-           
-        return res
-               
-            
-            
         
-       
