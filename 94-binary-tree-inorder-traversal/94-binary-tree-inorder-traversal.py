0e60aa35775e14c96416b5a83b2512579b7626fc
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-08-09 23:04:28 -0700

    Time: 37 ms (81.50%), Space: 13.8 MB (59.95%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -13,24 +13,25 @@ class Solution:
         
         
         stack=[]
+        
         if not root:
-            return stack
+            return []
         
         res=[]
+        
         while(stack or root):
             
             while(root):
                 stack.append(root)
                 root=root.left
-                
+               
             root=stack.pop()
             res.append(root.val)
             root=root.right
            
-       
+        
         return res
-            
-            
+                
             
             
             
