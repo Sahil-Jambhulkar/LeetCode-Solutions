6fc3931dc70e95bcf231e194d55ae6b9b2f72ea5
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-08-09 19:22:46 -0700

    Time: 44 ms (93.95%), Space: 15.3 MB (51.25%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -0,0 +1,35 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        
+        m={}
+        
+        for ch in s:
+            if ch in m:
+                m[ch]+=1
+               
+            else:
+                m[ch]=1
+               
+           
+        q=[]
+        
+        for ch in m:
+            q.append([-1*m[ch],ch])
+            
+        print(q)  
+        
+        heapq.heapify(q)
+        ans=''
+        while(q):
+            freq,ch=heapq.heappop(q)
+            print(ch)
+            ans+=ch*(freq*-1)
+           
+        
+        return ans
+            
+            
+           
+        
+            
+        
