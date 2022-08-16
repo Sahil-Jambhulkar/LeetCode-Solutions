390818c2393e35176b2ff6c5a9c9fee14394c12e
Sahil Jambhulkar <jambhulkarsahil@gmail.com>
2022-08-16 13:42:39 -0700

    Time: 64 ms (17.46%), Space: 14 MB (35.16%) - LeetHub

--- 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.py
+++ 951-flip-equivalent-binary-trees/951-flip-equivalent-binary-trees.py
@@ -9,18 +9,14 @@ class Solution:
         
         if not root1 and not root2:
             return True
-        
-        
         if not root1 or not root2:
             return False
         
-        if not(root1.val==root2.val):
+        
+        if root1.val!=root2.val:
             return False
         
         
-      
-        return (self.flipEquiv(root1.left,root2.left) or self.flipEquiv(root1.left,root2.right)) and (self.flipEquiv(root1.right,root2.left) or self.flipEquiv(root1.right,root2.right)) 
-    
-    
-    
-    
+        return (self.flipEquiv(root1.left,root2.left) or self.flipEquiv(root1.left,root2.right)) and (self.flipEquiv(root1.right,root2.left) or self.flipEquiv(root1.right,root2.right))  
+
+        
